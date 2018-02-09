ea28e7b4e59acdcec7cb2a84dab5b589a672638c
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2018-02-09 16:48:41 +0700

    Commit Sintaks Bab 3
    Memasukkan semua sintaks untuk Bab 3

---
+++ BAB 3/PengarangBuku.java
@@ -0,0 +1,20 @@
+public class PengarangBuku {
+    //atribut
+    public String nama;
+    public String tempatLahir;
+    public int umur;
+
+    //method
+    public void menulisbuku(){
+        //di sini isi perintah untuk menulis buku
+    }
+
+    public void mengirimkanNaskah(){
+        //di sini isi perintah untuk mengirimkan naskah
+    }
+    
+    public int berapaBuku(){
+        //di sini isi perintah untuk jumlah buku yang telah dibuat
+        return ...;
+    }
+}
