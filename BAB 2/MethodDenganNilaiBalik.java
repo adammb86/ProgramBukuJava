d9313931d9d8d571d1604d7dc6fd8215728c148e
Firman Nizammudin Fakhrul <firmannizammudin@gmail.com>
2018-02-15 04:21:44 +0700

    Update MethodDenganNilaiBalik.java

--- BAB 2/MethodDenganNilaiBalik.java
+++ BAB 2/MethodDenganNilaiBalik.java
@@ -1,8 +1,8 @@
 public class MethodDenganNilaiBalik {
     public static void main(String[] args) {
         int sisi = 4;
-        System.out.println("Luas Persegi dengan panjang sisi " + sisi + " 
-                            adalah " + hitungLuasPersegi(sisi));
+        System.out.println("Luas Persegi dengan panjang sisi " + sisi + 
+                           " adalah " + hitungLuasPersegi(sisi));
     }
 
     // Method dengan nilai balik untuk menghitung luas persegi
